5aa8493fa12a66006be6cae16e97ddfda7e2bfb8
Michael Cooper <macoop2363@gmail.com>
2019-12-20 10:33:35 -0700

    Rename spec file

--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe 'NRELService' do
 
     response = NRELService.get_electric_stations(location)
 
+    binding.pry
+
     expect(response[:total_results]).to eq(108)
     expect(response[:fuel_stations].first[:fuel_type_code]).to eq('ELEC')
     expect(response[:fuel_stations].first[:station_name]).to eq('Seventeenth Street Plaza')
