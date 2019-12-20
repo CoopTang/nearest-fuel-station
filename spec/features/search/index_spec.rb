dbaec5c68e307d170c1ff88751b26d523a69a5e0
Michael Cooper <macoop2363@gmail.com>
2019-12-20 09:24:10 -0700

    Add initial test for search route

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+# As a user
+# When I visit "/"
+# And I select "Turing" from the start location drop down (Note: Use the existing search form)
+# And I click "Find Nearest Station"
+# Then I should be on page "/search"
+# Then I should see the closest electric fuel station to me.
+# For that station I should see
+# - Name
+# - Address
+# - Fuel Type
+# - Access Times
+# I should also see:
+# - the distance of the nearest station (should be 0.1 miles)
+# - the travel time from Turing to that fuel station (should be 1min)
+# - The HTML direction instructions to get to that fuel station
+#   "Head <b>southeast</b> on <b>17th St</b> toward <b>Larimer St</b>"
+
+RSpec.describe 'As a user' do
+  describe 'when I visit "/" and select Turing from the start location drop down' do
+    before :each do
+      visit root_path
+
+      select 'Turing', from: :location
+
+      click_button 'Find Nearest Station'
+    end
+    it 'then I should be on /search' do
+      expect(current_path).to eq('/search')
+    end
+
+    it 'should see the closest electric fuel station to me info: (name, address, fuel type, access times' do
+
+    end
+
+    it 'should also show distance, travel time, and html direction instructions' do
+
+    end
+  end
+end
